6dbdc43856b1e6daf6d84cbab9208367be1094aa
Sanskar Mishra <sanskar.mishra@gmail.com>
2021-11-13 15:21:08 -0600

    added a createuser funciton that is to be connected to API

--- fttAPI/main.py
+++ fttAPI/main.py
@@ -1,10 +1,49 @@
-#Flask helps build API - allows website to access database
-from flask import Flask
+# Flask helps build API - allows website to access database
+from flask import Flask, request, jsonify
+
 app = Flask(__name__)
 
-@app.route("/",methods = ["GET"])
+
+@app.route("/", methods=["GET"])
 def hello_world():
     return "bye World"
+
+
+@app.route("/query", methods=["GET"])
+def query():
+    number = request.args.get("number")
+    return number + "1"
+
+
+@app.route("/somethining", methods=["GET"])
+def somethining():
+    body = request.json
+    if body is None:
+        return "No json provided"
+    number = body.get("num", 0)
+    text = body.get("text", "no text provided")
+    value = {
+        "newtext": text + text,
+        "newnumber": number + 5
+    }
+    return jsonify(value)
+
+@app.route("/createuser", methods=["POST"])
+def post():
+    body = request.json
+    if body is None:
+        return "Error: NO USER PROVIDED"
+    user = body.get("username", None)
+    password = body.get("password", None)
+
+    if user is None or password is None:
+        return "Error: INCOMPLETE CREDENTIALS"
+
+    #TODO Create user in database
+
+    return "SUCCESS: USER CREATED"
+
+
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=80)
